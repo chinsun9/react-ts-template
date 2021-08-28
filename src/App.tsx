55ddc61595e81c0011e3ede0c3a5f6357e1836d0
chinsun9 <chin_sung@naver.com>
2021-08-28 09:32:52 +0900

    delete App.css

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from 'react';
-import './App.css';
 
-function App() {
-  return (
-    <div>
-      @chinsun9/my-react-ts-template
-    </div>
-  );
-}
+const App = () => (
+  <div>
+    @chinsun9/my-react-ts-template
+  </div>
+);
 
 export default App;
