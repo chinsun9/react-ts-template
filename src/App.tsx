87473917d4f5cdf520cae5e44e862807c10a37e9
chinsun9 <chin_sung@naver.com>
2021-11-20 15:47:32 +0900

    add example

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Empty from 'src/components/Empty';
 
-const App = () => (
-  <div>
-    @chinsun9/react-ts-template
-  </div>
-);
+const App = () => {
+  return (
+    <>
+      <h1>@chinsun9/react-ts-template</h1>
+      <Empty />
+    </>
+  );
+};
 
 export default App;
